fd262355d29938e1c8d7f26454a40d3eb6c5dc59
mustbebuilt <m.j.cooper@shu.ac.uk>
2020-10-03 07:35:22 +0000

    Add allFilms demo

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -4,5 +4,19 @@ var ObjectId = require("mongodb").ObjectId;
 
 module.exports = {
     
+    viewAll: function(app, req, res){
+        console.info("Hello from the Controller")
+        app
+          .set("myDb")
+          .collection("filmsCollection")
+          .find({})
+          .toArray(function (err, docs) {
+            console.dir(docs);
+            return res.render("allFilms", {
+              title: "All Films",
+              films: docs,
+            });
+          });
+    }
 
 }
--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 // add myControllers 
-//const myControllers = require('../controllers/controllers.js');
+const myControllers = require('../controllers/controllers.js');
 
 //console.dir(myControllers);
 
@@ -12,11 +12,22 @@ module.exports = (app) => {
     router.get('/main', (req, res) => {
         return res.render('main', {
             title: 'EJS Example from Parts', 
-            message: 'Hello Template built in parts',
+            message: 'Hello Template built in parts two',
             showMsg: true,
             headingOne: 'Page made from parts'
             });
     });
+
+    router.get('/allfilms', (req, res) => {
+        console.info("All Films Called")
+        myControllers.viewAll(app, req, res);
+    })
+
+
+
+
+
+    
 
   
     return router;
